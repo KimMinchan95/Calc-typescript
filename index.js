38d8a4874e6e813ceb56329e68e1f0a4a6689315
KimMinchan95 <hialscks@gmail.com>
2021-11-05 00:02:18 +0900

    update: index.ts

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)('Enter first number:\n');
+    var operator = (0, readline_sync_1.question)('Enter operator:\n');
+    var secondStr = (0, readline_sync_1.question)('Enter second number:\n');
+    console.log(firstStr, operator, secondStr);
+}
+main();
---
+++ index.ts
@@ -0,0 +1,11 @@
+import { question } from 'readline-sync';
+
+function main(): void {
+  const firstStr: string = question('Enter first number:\n');
+  const operator: string = question('Enter operator:\n');
+  const secondStr: string = question('Enter second number:\n');
+
+  console.log(firstStr, operator, secondStr);
+}
+
+main();
